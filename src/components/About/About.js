fa4e39113531d981f3d7342e3463e245bfc355c0
Will March <willmarch11@hotmail.com>
2023-05-10 16:45:46 -0500

    added custom css and images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import image from '../../images/pic of me (2).jpeg'
+import './About.css'
 
 export default function About() {
   return (
-    <h1>About</h1>
+    <section className="about">
+      <article className="bg-purple p-4 mb-5 text-white">
+        <h1 className='text-center'>About the Developer</h1>
+      </article>
+      <Container>
+        <Row>
+          <Col lg={6} className='mt-5'>
+            <img src={image} alt='Spencer Pearson, React Web Developer' className='profilePic' />
+          </Col>
+          <Col lg={6} className='about-text mt-5'>
+            <h3>Welcome to my app!</h3>
+            <p>
+              Hey, I'm Will March. This ReactJS ToDo Site is one of many projects I've created during my time at Centriq training. To see other projects I've created, please see  <a href="github.com/willmarch1" target="_blank" rel="noreferrer" alt='My GitHub'>my github.</a>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
